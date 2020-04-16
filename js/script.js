8850348bcb62d417ca14ebf935edc93d7fbb870e
Daria Novikova <daniellaskill@gmail.com>
2020-04-16 22:21:01 +0300

    WIP. стилизовала полностью форму

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ $(document).ready(function() {
 	$(".j-trial-lesson-btn").click(function(){
 		$(".j-form-promo").toggleClass("form--active");
 		// $(".j-trial-lesson").slideToggle();
+		$("j-html-hide").addClass("html-hide--active");
+	})
+});
 
+$(document).ready(function() {
+	$(".j-form-closed-btn").click(function(){
+		$(".j-form-promo").removeClass("form--active");
+		$("j-html-hide").removeClass("html-hide--active");
 	})
 });
