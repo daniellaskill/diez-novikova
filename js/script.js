7577b216ea9be799da6518ba5d68fe0831ae5a63
Daria Novikova <daniellaskill@gmail.com>
2020-04-17 14:40:47 +0300

    WIP. исправила j-html-hide

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
 	})
 });
 
+
+// $(document).ready(function() {
+// 		$(".j-html-hide").removeClass("html-hide--active");
+// });
+
+
 $(document).ready(function() {
 	$(".j-trial-lesson-btn").click(function(){
 		$(".j-form-promo").toggleClass("form--active");
@@ -13,6 +19,7 @@ $(document).ready(function() {
 		$(".j-html-hide").addClass("html-hide--active");
 	})
 });
+
 
 $(document).ready(function() {
 	$(".j-form-closed-btn").click(function(){
@@ -36,57 +43,3 @@ $(document).ready(function() {
 
 
 
-
-// // Флаг включённости слайдера
-// var slickSliderActive = false;
-
-// // Включение или выключение слайдера (в зависимости от ширины)
-// function checkSlider(){    
-  
-//   // Если вьюпорт уже чем 768 
-//   if( $(window).width() < 1280 - getScroll() ) {
-    
-//     // Если флаг включённости опущен, то включим и поднимем флаг
-//     if(slickSliderActive == false) {
-//       $('#slider').slick({
-//         dots: false,
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       });
-//       slickSliderActive = true;
-//     }
-  
-//   } 
-//   // Иначе (вьюпорт НЕ уже чем 768)
-//   else {
-    
-//     // Если флаг включённости поднят, выключаем и опускаем флаг
-//     if(slickSliderActive == true) {
-//       $('#slider').slick('unslick');
-//       slickSliderActive = false;
-//     }
-    
-//   }
-// };
-
-// // По готовности DOM...
-// // $(document).ready(function(){
-//   checkSlider();
-// // });
-
-// // По любому изменению размера вьюпорта...
-// $(window).on('resize', function(){
-//    checkSlider();
-// });
-
-// function getScroll(){
-//   var div = document.createElement('slider');
-//   div.style.overflowY = 'scroll';
-//   div.style.width = '50px';
-//   div.style.height = '50px';
-//   div.style.visibility = 'hidden';
-//   document.body.appendChild(div);
-//   var scrollWidth = div.offsetWidth - div.clientWidth;
-//   document.body.removeChild(div);
-//   return scrollWidth;
-// }
